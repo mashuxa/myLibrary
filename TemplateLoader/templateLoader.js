c13418a6d7b55a9eacffb11864a55aa621244bf7
mashuxa <mashuxa@tut.by>
2018-08-07 13:00:03 +0300

    fixed problem with the order of loading js script, added polyfill for promise

--- TemplateLoader/templateLoader.js
+++ TemplateLoader/templateLoader.js
@@ -1,45 +1,73 @@
+"use strict";
+
 //Script use es6. You can use "babeljs" for es5.
 //Use tag "templ" (or raplace it in constant "TAG") 
 //with attribute "src='hereTemplateSrc'".
 //For example <templ src="components/header.html"><templ>
 //And include this script in header 
 //with attribute "defer" before all scripts.
+//Also you can set src for scripts in array
 (function () {
-    const TAG = "templ";
-    const ATTRIBUTE = "src";
+    document.addEventListener('DOMContentLoaded', function () {
+        var scriptsSrc = [
+            "design/js/jquery-3.3.1.min.js",
+            "design/js/slick.js",
+            "design/js/mobile-menu.js",
+            "design/js/mobile.js"];
+        var TAG = "templ";
+        var ATTRIBUTE = "src";
 
-    function getHTML(src) {
-        return new Promise(resolve => {
-            let xhr = new XMLHttpRequest();
-            xhr.open("GET", src, true);
-            xhr.send();
-            xhr.onreadystatechange = () => {
-                if (xhr.readyState !== 4) {
-                    return;
-                }
-                if (xhr.status !== 200) {
-                    console.error(xhr.status + ': ' + xhr.statusText);
-                }
-                else {
-                    resolve(xhr.responseText);
-                }
-            };
-        });
-    }
-    function replaceAllTags() {
-        let templTag = document.getElementsByTagName(TAG)[0];
-        if (templTag) {
-            let src = templTag.getAttribute(ATTRIBUTE);
-            getHTML(src).then(content => {
-                templTag.insertAdjacentHTML('beforebegin', content);
-            }).then(() => {
-                templTag.parentNode.removeChild(templTag);
-                replaceAllTags();
+        function getHTML(src) {
+            return new Promise(function (resolve) {
+                var xhr = new XMLHttpRequest();
+                xhr.open("GET", src, true);
+                xhr.send();
+                xhr.onreadystatechange = function () {
+                    if (xhr.readyState !== 4) {
+                        return;
+                    }
+                    if (xhr.status !== 200) {
+                        console.error(xhr.status + ': ' + xhr.statusText);
+                    } else {
+                        resolve(xhr.responseText);
+                    }
+                };
             });
         }
-        else {
-            return;
+
+        function replaceAllTags() {
+            var templTag = document.getElementsByTagName(TAG)[0];
+            if (templTag) {
+                var src = templTag.getAttribute(ATTRIBUTE);
+                getHTML(src).then(function (content) {
+                    templTag.insertAdjacentHTML('beforebegin', content);
+                    templTag.parentNode.removeChild(templTag);
+                    replaceAllTags();
+                });
+            } else {
+                var counter = 0;
+
+                function loadJS(fileSrc) {
+                    new Promise(function (resolve) {
+                        var script = document.createElement("script");
+                        script.src = fileSrc;
+                        script.setAttribute("defer", "");
+                        document.body.appendChild(script);
+                        counter++;
+                        script.onload = resolve;
+                    }).then(function () {
+                        if (counter === scriptsSrc.length) {
+                            return;
+                        } else {
+                            loadJS(scriptsSrc[counter]);
+                        }
+                    });
+                }
+                if (scriptsSrc.length) {
+                    loadJS(scriptsSrc[counter]);
+                }
+            }
         }
-    }
-    replaceAllTags();
-}());
+        replaceAllTags();
+    });
+})();
